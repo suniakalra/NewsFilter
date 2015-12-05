0d61c6fe0e6f938a8d0ad128d859d94c685d02da
Sunia Kalra <kalrasunia@gmail.com>
2015-12-05 18:09:15 +0530

    remove the first_sea_report model.

--- app/models/first_sea_report.rb
+++
@@ -1,4 +0,0 @@
-class FirstSeaReport < ActiveRecord::Base
-
-  has_many :sea_reports
-end
--- db/migrate/20151128125753_create_first_sea_reports.rb
+++
@@ -1,8 +0,0 @@
-class CreateFirstSeaReports < ActiveRecord::Migration
-  def change
-    create_table :first_sea_reports do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
